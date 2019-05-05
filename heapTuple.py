54d260944afb02add54d186807714c3eb9111750
Sam Grumley <grumsamu@gmail.com>
2019-05-06 04:41:41 +1000

    Soloution found, needs to be optimised in the case that summed value has duplicates

--- heapTuple.py
+++ heapTuple.py
@@ -6,38 +6,37 @@ class BinaryHeap(object):
         return len(self.items) - 1
 
     #parent node = node position / 2
-    def upHeap(self):
+    def upHeap(self, person):
         i = len(self)
         while i // 2 > 0:
-            if self.items[i] > self.items[i // 2]:
+            if self.items[i][person] > self.items[i // 2][person]:
                 self.items[i // 2], self.items[i] = self.items[i], self.items[i // 2]
             i = i // 2
 
-    def insert(self, item):
+    def insert(self, item, person):
         self.items.append(item)
-        self.upHeap()
+        self.upHeap(person)
 
     #
-    def downHeap(self, i):
+    def downHeap(self, i, person):
         while i * 2 <= len(self):
-            mc = self.min_child(i)
-            if self.items[i] < self.items[mc]:
+            mc = self.min_child(i, person)
+            if self.items[i][person] < self.items[mc][person]:
                 self.items[i], self.items[mc] = self.items[mc], self.items[i]
             i = mc
 
-    def min_child(self, i):
+    def min_child(self, i,person ):
         if i * 2 + 1 > len(self):
             return i * 2
-
-        if self.items[i * 2] > self.items[i * 2 + 1]:
+        if self.items[i * 2][person] > self.items[i * 2 + 1][person]:
             return i * 2
         return i * 2 + 1
 
-    def delete_min(self):
+    def delete_min(self,person):
         return_value = self.items[1]
         self.items[1] = self.items[len(self)]
         self.items.pop()
-        self.downHeap(1)
+        self.downHeap(1, person)
         return return_value
 
     def buildHeap(self, alist):
@@ -47,67 +46,14 @@ class BinaryHeap(object):
             self.downHeap(i)
             i = i - 1
 
-    def buildHeapTup(self):
-        i = len(self) // 2
-        while i > 0:
-            self.downHeapTup(i)
-            i = i - 1
-
-    def downHeapTup(self, i):
-        while i * 2 <= len(self):
-            mc = self.min_childTup(i)
-            if self.items[i][1] < self.items[mc][1]:
-                self.items[i], self.items[mc] = self.items[mc], self.items[i]
-            i = mc
-
-    def min_childTup(self, i):
-        if i * 2 + 1 > len(self):
-            return i * 2
-        if self.items[i * 2][1] < self.items[i * 2 + 1][1]:
-            return i * 2
-
-        return i * 2 + 1
-
-    def delete_minTup(self):
-        return_value = self.items[1]
-        self.items[1] = self.items[len(self)]
-        self.items.pop()
-        self.downHeapTup(1)
-        return return_value
-#tests to ensure max heap
-
-info = [ ( 1, 2), (3, 4), (5, 6) ]
-
-heaptest = BinaryHeap()
-heaptest.insert((98, 17))
-heaptest.insert((9, 23))
-heaptest.insert((99, 25))
-heaptest.insert((1000, 3))
-heaptest.insert((45, 30))
-print(heaptest.items)
-#heaptestShuffle = BinaryHeap()
-heaptestShuffle = heaptest
-print("shuffle",heaptestShuffle.items)
-#heaptestShuffle.buildHeapTup()
-for i in range(1,len(heaptestShuffle)):
-    heaptestShuffle.downHeapTup(i)
-print("sorted",heaptestShuffle.items)
-print(heaptest.delete_minTup())
-print(heaptest.delete_minTup())
-print(heaptest.delete_minTup())
 """
-heaptest = BinaryHeap()
-heaptest.insert(98)
-heaptest.insert(99)
-heaptest.insert(1000)
-
-print(len(heaptest))
-print(heaptest.items)
-print(heaptest.delete_min())
-print(heaptest.delete_min())
-
-lol = BinaryHeap()
-test = [11,2,44,54,3,5,677,8,8,65,4,3,2]
-lol.buildHeap(test)
-print(heaptest.items)
+#how to change priority
+for i in range(1,len(heaptestShuffle)):
+    heaptestShuffle.upHeap(person)
+print(heaptestShuffle.items)
+person = 0
+#how to change priority
+for i in range(1,len(heaptestShuffle)):
+    heaptestShuffle.upHeap(person)
+print(heaptestShuffle.items)
 """
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 
 import heap
+import heapTuple
 
 
 file = open("input.txt", "r")
 i = 0
 for line in file:
+    """
     print(line)
     if i == 0:
         numTests = line.split()[0]
     if i == 1:
         placehold = line.split()
-        n,k = placehold[0],placehold[1]
+        n,k = int(placehold[0]),int(placehold[1])
     if i == 2:
         data = line.split()
     if i == 3:
+        gofirst = line.split()[0]
+    """
+    if i == 0:
+        numTests = line.split()[0]
+    if i == 10:
+        placehold = line.split()
+        n,k = int(placehold[0]),int(placehold[1])
+    if i == 11:
+        data = line.split()
+    if i == 12:
         gofirst = line.split()[0]
     #call function to compute
     i+=1
@@ -24,35 +36,50 @@ print("Number of balls",n,"Max allowed turns",k)
 print("values on the balls",data)
 print("result of the coint toss",gofirst)
 
+tupheap = heapTuple.BinaryHeap()
+
 #finds a prioritylist for rusty
-summedResults =[]
+#summedResults =[]
+person = 0
+if gofirst == 'TAILS':
+    person = 1
+
+print("1 = rusty, 0 = Scott")
+print("person: ",person)
+
 for i in range(len(data)):
     sumVal = 0
     for j in range(len(data[i])):
         sumVal += int(data[i][j])
-    summedResults.append(sumVal)
-print("summed results",summedResults)
+    print(data[i], sumVal)
+    tupheap.insert((int(data[i]), sumVal),person)
 
-rustyBalls = heap.BinaryHeap()
-rustyBalls.buildHeap(summedResults)
-print(rustyBalls.items)
+print("initial dataset",tupheap.items)
+playerSum = [0,0]
+while(len(tupheap)>0):
+    for i in range(k):
+        if len(tupheap)>0:
+            #test
+            if person == 1 and tupheap.delete_min(1)[1] == 13:
+                print("whats the orderrerererr",tupheap.items)
+            #pull all numbers until there is a difference take the biggest value and insert the rest
+            #or when looking for the max compare values as well
+            #test
+
+            if i+1 == k:
+                toSeewhatHappening = tupheap.delete_min(1 - person)[0]
+            else:
+                toSeewhatHappening = tupheap.delete_min(person)[0]
+            print("Choice:",toSeewhatHappening)
+            playerSum[person] += toSeewhatHappening
+    person = 1-person
+    print("change in turn",person)
 
 
-results = list(map(int, data))
-scottBalls = heap.BinaryHeap()
-scottBalls.buildHeap(results)
-print(scottBalls.items)
+print(playerSum)
 
-scottSum = 0
-rustySum = 0
+
 """
-#have an array that is ordered by the first person?
-if gofirst == "Tails":
-    rustySum += balls.delete_min()
-    #Rusty starts
-else:
-    scottSum += balls.delete_min()
-    #Scott starts
-print("Scott:", scottSum)
-print("rusty", rustySum)
+somehow take the largest if there are multiple
+error handling for delete_min
 """
